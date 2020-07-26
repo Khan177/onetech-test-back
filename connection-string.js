0c8b18910ffa15bb222f2123add4c888cd06f962
Adilkhan Nurgaliyev <khan17171717@gmail.com>
2020-07-27 00:19:23 +0600

    finish

--- connection-string.js
+++
@@ -1,5 +0,0 @@
-const connectionString =
-  "mongodb+srv://khanter:ebash@cluster0.eqpta.mongodb.net/onetech?retryWrites=true&w=majority";
-exports.connectionString = () => {
-  return connectionString;
-};
